8c183d2e267d5b408a731b25eaa92baf3cc638e4
Krittipong Kanchanapiboon <krittipong@ava.fund>
2020-08-06 14:31:02 +0700

    fix bug search button

--- vis.js
+++ vis.js
@@ -378,14 +378,5 @@ async function draw(symbol) {
 async function main() {
   draw('BBL.BK')
   drawInfo('BBL.BK')
-
-  $("form").submit(function (e) {
-    e.preventDefault();
-    var symbol = $("#symbolSearch").val()
-    drawInfo(symbol)
-    draw(symbol)
-    
-    
-  });
 }
 main()
